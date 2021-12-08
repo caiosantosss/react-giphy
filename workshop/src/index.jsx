f3f76b5383a5699d463574a5ce4bfcc46ed24272
Caio Santos <caio.santos@msn.com>
2021-12-08 22:05:10 +0900

    Updates

--- workshop/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import '../assets/stylesheets/application.scss';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App';
-
-const root = document.getElementById('root');
-ReactDOM.render(<App />, root);
